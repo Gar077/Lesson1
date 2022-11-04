e19526a7f83456d09b15e399889d9861c58ced21
Gar077 <grigoryan.gar2006@gmail.com>
2022-11-04 17:53:30 +0400

    chnges

---
+++ hrant.js
@@ -0,0 +1,50 @@
+// every - veradarcnum e true ete funkciayum bolory paymanin bavararuma ete vochuremn false 
+
+// var arr = [4, -10, 7, 17, 6];
+// var x = arr.every(f);
+// function f(item){
+//     return item < 100;
+// }
+// console.log(x);
+// alert(x);
+
+// var arr = [4, -10, 72, 17, 6];
+// function customEvery(arr, f) {
+//     for (var i = 0; i < arr.length; i++) {
+//         if (!f(arr[i])) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// function f(item) {
+//     return item < 100;
+// }
+// console.log(customEvery(arr, f));
+
+// some - ashxatume everyi nman bayc ete tveric meky hamapatasxanume paymanin tpume true
+
+// var arr = [400, 10, 720, 170, 600];
+// function customSome(arr, f) {
+//     for (var i = 0; i < arr.length; i++){
+//         if (f(arr[i])) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// function f(item) {
+//     return item < 100;
+// }
+// console.log(customSome(arr, f));
+
+// map - amen andam bazmapatkume 10-ov 
+
+// var arr = [4, 1, 7, 17, 60];
+// function customMap(arr) {
+//     for (var i = 0; i < arr.length; i++){
+//     arr[i] *= 10;
+//     }
+//     return arr
+// }
+// console.log(customMap(arr))
